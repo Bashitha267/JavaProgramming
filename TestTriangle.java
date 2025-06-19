90dc13a1012f3534b02959b7e321a4e9a74c1e0b
= <DCSUser@cn.lan>
2025-06-19 10:00:32 +0530

    Add an anchor for the trial end section

---
+++ TestTriangle.java
@@ -0,0 +1,6 @@
+public class TestTriangle{
+	public static void main(String args[]){
+		Triangle T1=new Triangle();
+		System.out.println(T1.findArea(3,4));
+	}
+}
---
+++ Triangle.java
@@ -0,0 +1,8 @@
+public class Triangle{
+	public Triangle(){
+		
+	}
+	public double findArea(int a,int b){
+		return (a*b)/2;
+	}
+}
