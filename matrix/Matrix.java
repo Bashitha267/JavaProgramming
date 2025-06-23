b4e8963f1bc5a3f4fd8d79d9bf4e0a8cb0da6736
Nimesh <nimeshspc2k17.gmail.com>
2025-06-23 11:27:01 +0530

    added

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -9,7 +9,26 @@ public class Matrix {
         matrix = new int[rows][cols];
     }
 
+    Matrix(int m[][]) {
+        matrix = m;
+
+    }
+
     public void setElements(int i, int j, int value) {
         matrix[i][j] = value;
+    }
+
+    public int getElement(int i, int j) {
+        return matrix[i][j];
+    }
+
+    public int[][] AddMatrix(Matrix M2) {
+        int[][] sum = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                sum[i][j] = matrix[i][j] + M2.getElement(i, j);
+            }
+        }
+        return sum;
     }
 }
--- matrix/TestMatrix.java
+++ matrix/TestMatrix.java
@@ -1,6 +1,27 @@
 public class TestMatrix {
     public static void main(String[] args) {
-        Matrix M1 = new Matrix(2, 3);
-        Matrix M2 = new Matrix(2, 3);
+        // Matrix M1 = new Matrix(1, 2);
+        // Matrix M2 = new Matrix(1, 2);
+        Matrix M1 = new Matrix(
+                new int[][] {
+                        { 1, 2, 3 }, { 4, 5, 6 }
+                }
+
+        );
+        Matrix M2 = new Matrix(
+                new int[][] {
+                        { 1, 2, 3 }, { 4, 5, 6 }
+                }
+
+        );
+        int[][] sum = M1.AddMatrix(M2);
+        for (int i = 0; i < sum.length; i++) {
+            for (int j = 0; j < sum[0].length; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
+
 }
