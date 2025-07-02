7636042afdec86f2d974c27b5f9d7489c787f1a3
DCS User <DCSUser@cn.lan>
2025-07-02 22:05:21 +0530

    added CheckSorted

--- Arrays/CheckSorted.java
+++ Arrays/CheckSorted.java
@@ -1,13 +1,21 @@
 public class CheckSorted{
 	public static boolean check(int arr[]){
-		for (int i=0;i<arr.length-1;i++){
-			if(arr[i]>arr[i+1])
+		for (int i=0;;){
+				if(i==arr.length-1){
+					break;
+				}
+				if(arr[i]>arr[i+1])
 				return false;
+			
+			
+			i=i+1;
 		}
 		return true;
 	}
 	public static void main(String args[]){
 		int arr[]={1,5,6,7,2};
+		int arr1[]={1,5,6,7,7,8};
 		System.out.println(check(arr));
+		System.out.println(check(arr1));
 	}
 }
