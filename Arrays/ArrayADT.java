838e3c7342d408b3ea361ec927feb40fafe36c84
DCS User <DCSUser@cn.lan>
2025-07-02 23:27:01 +0530

    Resolve merge conflicts

--- Arrays/ArrayADT.java
+++ Arrays/ArrayADT.java
@@ -19,7 +19,17 @@ public class ArrayADT{
 		System.out.println("}");
 	System.out.println();
 		}
-	
+	public static void delete(int index){
+		if(index<0||index>size-1){
+			throw new IndexOutOfBoundsException("Index is out of range");
+		}
+		else {
+			for(int i=index;i<size-1;i++){
+				array[i]=array[i+1];
+			}
+			size--;
+		}
+	}
 	public static void Insert(int index,int value){
 		if(index<0||index>array.length-1){
 			throw new IndexOutOfBoundsException("Index is out of range");
@@ -36,6 +46,32 @@ public class ArrayADT{
 		}
 		
 	}
+	public static void getElement(int index){
+		if(index<0||index>=size){
+			throw new IndexOutOfBoundsException("Index is out of range");
+		}
+		else {
+			for(int i=0;i<size;i++){
+			if(i==index)
+				System.out.println(array[i]);
+		}
+		}
+		
+		
+	}
+		public static void setElement(int index,int value){
+		if(index<0||index>=size){
+			throw new IndexOutOfBoundsException("Index is out of range");
+		}
+		else {
+	array[index]=value;
+		}
+		
+		
+	}
+	public static int getLength(){
+		return size;
+	}
 	
 	public static void main(String args[]){
 		ArrayADT arr=new ArrayADT(5);
@@ -44,6 +80,8 @@ public class ArrayADT{
 		Insert(2,4);
 		Insert(3,2);
 		Insert(4,1);
+		delete(2);
+		setElement(1,15);
 		traverse();
 
 		
