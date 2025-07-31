9b7380b401ce6bb6f88405af333453a4447e69c5
Nimesh <nimeshspc2k17.gmail.com>
2025-08-01 00:45:28 +0530

    added circular

---
+++ CircularLinkedList/LinkedList.java
@@ -0,0 +1,56 @@
+public class LinkedList {
+    public Node tail;
+    public int size;
+
+    public LinkedList() {
+        tail = null;
+    }
+
+    // insertion at begining
+    public void add(int value) {
+        Node node = new Node(value);
+        if (tail == null) {
+            tail = node;
+            tail.next = tail;
+            size++;
+        } else {
+            node.next = tail.next;
+            tail.next = node;
+            size++;
+        }
+
+    }
+
+    // insertion at middle
+    public void add(int position, int value) {
+        Node node = new Node(value);
+        Node current = tail.next;
+        if (size == 0) {
+            add(value);
+            return;
+        }
+        if (position >= size) {
+            System.out.println("Enter valid position to insert");
+            return;
+        }
+        for (int i = 0; i < size; i++) {
+            if (i == position - 1) {
+                node.next = current.next;
+                current.next = node;
+                size++;
+                return;
+            }
+            current = current.next;
+        }
+
+    }
+
+    public void print() {
+        Node currentValue = tail.next;
+        for (int i = 0; i < size; i++) {
+            System.out.println(currentValue.value);
+            currentValue = currentValue.next;
+        }
+        // System.out.println(currentValue.value);
+    }
+}
---
+++ CircularLinkedList/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        LinkedList L1 = new LinkedList();
+        L1.add(5);
+        L1.add(45);
+        L1.add(70);
+        L1.add(150);
+        // L1.add(2, 200);
+        L1.print();
+    }
+}
